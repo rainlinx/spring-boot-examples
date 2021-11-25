439285cd0c54b9068a6e8125576e90ee1736a906
rainlin <472016475@qq.com>
2021-11-25 13:51:50 +0800

    添加rabbitmq延迟队列示例

--- spring-boot-rabbitmq/src/main/java/com/neo/rabbit/RabbitConfig.java
+++ spring-boot-rabbitmq/src/main/java/com/neo/rabbit/RabbitConfig.java
@@ -1,12 +1,33 @@
 package com.neo.rabbit;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PostConstruct;
 
+@Slf4j
 @Configuration
 public class RabbitConfig {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public RabbitConfig(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    /**
+     * 设置消息的回调函数，需在配置文件中开启相应配置后生效
+     * ConfirmCallback：消息成功发送至exchange后的回调函数
+     * ReturnCallback：消息未成功发送至queue后的回调函数
+     */
+    @PostConstruct
+    public void init() {
+        this.rabbitTemplate.setConfirmCallback(confirmCallback());
+        this.rabbitTemplate.setReturnCallback(returnCallback());
+    }
 
     @Bean
     public Queue helloQueue() {
@@ -23,5 +44,13 @@ public class RabbitConfig {
         return new Queue("object");
     }
 
+    @Bean
+    public RabbitTemplate.ConfirmCallback confirmCallback() {
+        return (correlationData, ack, cause) -> log.info("消息传递到了exchange");
+    }
 
+    @Bean
+    public RabbitTemplate.ReturnCallback returnCallback() {
+        return (message, replyCode, replyText, exchange, routingKey) -> log.info("消息未传递到queue");
+    }
 }
--- spring-boot-rabbitmq/src/main/java/com/neo/rabbit/hello/HelloSender.java
+++ spring-boot-rabbitmq/src/main/java/com/neo/rabbit/hello/HelloSender.java
@@ -1,7 +1,6 @@
 package com.neo.rabbit.hello;
 
 import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -16,7 +15,7 @@ public class HelloSender {
 	public void send() {
 		String context = "hello " + new Date();
 		System.out.println("Sender : " + context);
-		this.rabbitTemplate.convertAndSend("hello", context);
+		this.rabbitTemplate.convertAndSend("hello1", context);
 	}
 
 }
